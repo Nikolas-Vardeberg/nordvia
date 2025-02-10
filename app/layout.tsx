c918cd5c45aedb9fb9b31dfc052bbff540869376
Nikolas, Vardeberg - Rek <156543836+Nikolas-Vardeberg@users.noreply.github.com>
2025-02-10 20:01:32 +0100

    feat: aeronaut local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const aeronaut = localFont({
+  variable: "--font-aeronaut",
+  src: "./fonts/Aeronaut.woff2"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${aeronaut.variable} antialiased`}
       >
         {children}
       </body>
